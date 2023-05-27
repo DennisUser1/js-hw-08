50f2d7fcaab3919f4be8d39abb4f59e3f4da0a4c
DennisUser1 <dennis3ace@gmail.com>
2023-05-27 22:19:47 +0300

    bug fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -58,4 +58,56 @@ function onFormSubmit(evt) {
     }
   } 
 
- 
+// import throttle from 'lodash.throttle';
+
+// const formEl = document.querySelector('.feedback-form');
+// const STORAGE_KEY = 'feedback-form-' + formEl.id;
+// const inputElements = formEl.querySelectorAll('input, textarea');
+
+// formEl.addEventListener('submit', onFormSubmit);
+// formEl.addEventListener('input', throttle(onFormInput, 500));
+
+// onFormFulfill();
+
+// function onFormFulfill() {
+//   const formValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (formValue !== null) {
+//     setFormValueOnLoad(formValue);
+//   }
+// }
+
+// function setFormValueOnLoad({ email, message } = {}) {
+//   inputElements.forEach((element) => {
+//     if (element.name === 'email') {
+//       element.value = email || '';
+//     } else if (element.name === 'message') {
+//       element.value = message || '';
+//     }
+//   });
+// }
+
+// function onFormInput() {
+//   const formValue = getFormValue();
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+// }
+
+// function getFormValue() {
+//   const formValue = {};
+//   inputElements.forEach((element) => {
+//     formValue[element.name] = element.value;
+//   });
+//   return formValue;
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+//   const formValue = getFormValue();
+
+//   if (formValue.email && formValue.message) {
+//     console.log(formValue);
+//     formEl.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//   } else {
+//     console.log('Будь ласка, заповніть обидва поля');
+//   }
+// }
