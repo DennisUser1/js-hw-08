3e2651a419240f74e5c3bc0cfbca526b68de7e6e
DennisUser1 <dennis3ace@gmail.com>
2023-05-28 17:48:43 +0300

    bug fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,65 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-// const emailEl = document.querySelector('.feedback-form input');
-// const messageEl = document.querySelector('.feedback-form textarea');
-// const STORAGE_KEY = 'feedback-form-state';
 const STORAGE_KEY = 'feedback-form-' + formEl.id;
 
-
 formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('change', throttle(onFormInput, 500));
+formEl.elements.email.addEventListener('input', onEmailInput); 
 
 onFormFulfill();
+populateInputsArea();
 
 function onFormFulfill() {
-    const formValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (formValue !== null) {
-    // if (formValue === null) {
-    // return;
+  const formValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formValue !== null) {
     setFormValueOnLoad(formValue);
-    }
+  }
 }
 
 function setFormValueOnLoad({ email, message } = {}) {
-    if (email && message) {
-      formEl.elements.email.value = email;
-      formEl.elements.message.value = message;
-    } else {
-        formEl.elements.email.value = '';
-        formEl.elements.message.value = '';
-    }
+  formEl.elements.email.value = email || '';
+  formEl.elements.message.value = message || '';
 }
-  
+
 function onFormInput() {
-    const formValue = getFormValue();
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+  const formValue = getFormValue();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+}
+
+function onEmailInput() {
+  const formValue = getFormValue();
+  formValue.email = formEl.elements.email.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
 }
 
 function getFormValue() {
-    const email = formEl.elements.email.value;
-    const message = formEl.elements.message.value;
-    return { email, message };
+  const email = formEl.elements.email.value;
+  const message = formEl.elements.message.value;
+  return { email, message };
 }
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    const formValue = getFormValue();
-    
-    if (formValue.email && formValue.message) {
-      console.log(formValue);
-      
-      // evt.currentTarget.reset();
-      formEl.reset();
-      localStorage.removeItem(STORAGE_KEY);
-    } else {
-      // Виконується, якщо хоча б одне з полів не заповнено
-      console.log('Будь ласка, заповніть обидва поля');
-    }
-  } 
+  evt.preventDefault();
+  const formValue = getFormValue();
+
+  if (formValue.email && formValue.message) {
+    console.log(formValue);
+
+    formEl.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  } else {
+    console.log('Будь ласка, заповніть обидва поля');
+  }
+}
+
+function populateInputsArea() {
+  const formValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formValue !== null) {
+    setFormValueOnLoad(formValue);
+  }
+}
+
 
 // import throttle from 'lodash.throttle';
 
