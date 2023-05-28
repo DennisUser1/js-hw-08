5792ee572a400dbc0021405e4144b4efb77257c5
DennisUser1 <dennis3ace@gmail.com>
2023-05-28 23:42:29 +0300

    localStorage save message

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,7 +5,8 @@ const STORAGE_KEY = 'feedback-form-' + formEl.id;
 
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('change', throttle(onFormInput, 500));
-formEl.elements.email.addEventListener('input', onEmailInput); 
+formEl.elements.email.addEventListener('input', onEmailInput);
+formEl.elements.message.addEventListener('input', onMessageInput);
 
 onFormFulfill();
 populateInputsArea();
@@ -30,6 +31,12 @@ function onFormInput() {
 function onEmailInput() {
   const formValue = getFormValue();
   formValue.email = formEl.elements.email.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+}
+
+function onMessageInput() {
+  const formValue = getFormValue();
+  formValue.message = formEl.elements.message.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
 }
 
@@ -61,6 +68,68 @@ function populateInputsArea() {
 }
 
 
+// import throttle from 'lodash.throttle';
+
+// const KEY_FOR_STORE = 'feedback-form-state';
+// const form = document.querySelector('.feedback-form');
+
+// readDataFromLocalStorage();
+
+// form.addEventListener('submit', onSubmitForm);
+// form.addEventListener('input', throttle(onInputForm, 500));
+
+
+// function onInputForm(event) {
+//   const { name, value } = event.target;
+//   localStorage.setItem(`${KEY_FOR_STORE}-${name}`, value);
+// }
+
+// function onSubmitForm(event) {
+//   event.preventDefault();
+  
+//   const emailInput = document.getElementsByName('email')[0];
+//   const messageInput = document.getElementsByName('message')[0];
+
+//   if (!emailInput.value || !messageInput.value) {
+//     alert('Please enter your email address and message!');
+//     return false;
+//   }
+
+//   console.log(getFormData());
+
+//   form.reset();
+//   clearFormData();
+// }
+
+// function readDataFromLocalStorage() {
+//   const email = localStorage.getItem(`${KEY_FOR_STORE}-email`);
+//   const message = localStorage.getItem(`${KEY_FOR_STORE}-message`);
+
+//   if (email) {
+//     const emailInput = document.getElementsByName('email')[0];
+//     emailInput.value = email;
+//   }
+
+//   if (message) {
+//     const messageInput = document.getElementsByName('message')[0];
+//     messageInput.value = message;
+//   }
+// }
+
+// function clearFormData() {
+//   localStorage.removeItem(`${KEY_FOR_STORE}-email`);
+//   localStorage.removeItem(`${KEY_FOR_STORE}-message`);
+// }
+
+// function getFormData() {
+//   const email = document.getElementsByName('email')[0].value;
+//   const message = document.getElementsByName('message')[0].value;
+//   return { email, message };
+// }
+
+
+
+// Перший варіант
 // import throttle from 'lodash.throttle';
 
 // const formEl = document.querySelector('.feedback-form');
